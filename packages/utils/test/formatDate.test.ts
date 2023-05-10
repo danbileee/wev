57d963ea3604d4d7b4f07d130a1b918d244dd0bf
danbileee <hello@danbileee.com>
2023-05-10 21:11:35 +0900

    Feature/monorepo (#6)
    * test: ci/cd push fail
    
    * fix: workflow trigger

--- packages/utils/test/formatDate.test.ts
+++ packages/utils/test/formatDate.test.ts
@@ -2,11 +2,11 @@ import formatDate from '../src/date-fns/formatDate';
 
 describe('formatDate', () => {
   test('날짜를 한국어식으로 포맷팅합니다.', () => {
-    expect(formatDate(new Date('2023-05-01'), 'KO')).toBe('2023/05/01');
+    expect(formatDate(new Date('2023-05-01'), 'SLASH_KO')).toBe('2023/05/01');
   });
 
   test('날짜를 영어식으로 포맷팅합니다.', () => {
-    expect(formatDate(new Date('2023-05-01'), 'EN')).toBe('05/01/2023');
+    expect(formatDate(new Date('2023-05-01'), 'SLASH_EN')).toBe('05/01/2023');
   });
 
   test('날짜를 구분자 없이 8자리로 표시합니다.', () => {
