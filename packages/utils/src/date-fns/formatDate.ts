dc56f2cd111847d6f2110fe08ccd401fc4dc7a57
Danbi Lee <hello@danbileee.com>
2023-05-10 22:14:30 +0900

    feat: add new date format cases

--- packages/utils/src/date-fns/formatDate.ts
+++ packages/utils/src/date-fns/formatDate.ts
@@ -5,6 +5,8 @@ const DateFormats = {
   SLASH_EN: 'MM/dd/yyyy',
   DOT_KO: 'yyyy.MM.dd',
   DOT_EN: 'MM.dd.yyyy',
+  HYPHEN_KO: 'yyyy-MM-dd',
+  HYPHEN_EN: 'MM-dd-yyyy',
   NO_DELIMITER_8: 'yyyyMMdd',
   NO_DELIMITER_6: 'yyMMdd',
 } as const;
--- packages/utils/test/formatDate.test.ts
+++ packages/utils/test/formatDate.test.ts
@@ -17,6 +17,14 @@ describe('formatDate', () => {
     expect(formatDate(new Date('2023-05-01'), 'DOT_EN')).toBe('05.01.2023');
   });
 
+  test('날짜를 "-" 구분자 넣어 한국어식으로 포맷팅합니다.', () => {
+    expect(formatDate(new Date('2023/05/01'), 'HYPHEN_KO')).toBe('2023-05-01');
+  });
+
+  test('날짜를 "-" 구분자 넣어 영어식으로 포맷팅합니다.', () => {
+    expect(formatDate(new Date('2023.05.01'), 'HYPHEN_EN')).toBe('05-01-2023');
+  });
+
   test('날짜를 구분자 없이 8자리로 표시합니다.', () => {
     expect(formatDate(new Date('2023-05-01'), 'NO_DELIMITER_8')).toBe('20230501');
   });
