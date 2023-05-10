48e839ace17bb7bdebae47213d3506fdaaa42783
Danbi Lee <hello@danbileee.com>
2023-05-10 21:18:32 +0900

    fix: include merged tag

--- packages/utils/test/formatDate.test.ts
+++ packages/utils/test/formatDate.test.ts
@@ -1,12 +1,20 @@
 import formatDate from '../src/date-fns/formatDate';
 
 describe('formatDate', () => {
-  test('날짜를 한국어식으로 포맷팅합니다.', () => {
+  test('날짜를 "/" 구분자 넣어 한국어식으로 포맷팅합니다.', () => {
     expect(formatDate(new Date('2023-05-01'), 'SLASH_KO')).toBe('2023/05/01');
   });
 
-  test('날짜를 영어식으로 포맷팅합니다.', () => {
+  test('날짜를 "/" 구분자 넣어 영어식으로 포맷팅합니다.', () => {
     expect(formatDate(new Date('2023-05-01'), 'SLASH_EN')).toBe('05/01/2023');
+  });
+
+  test('날짜를 "." 구분자 넣어 한국어식으로 포맷팅합니다.', () => {
+    expect(formatDate(new Date('2023-05-01'), 'DOT_KO')).toBe('2023.05.01');
+  });
+
+  test('날짜를 "." 구분자 넣어 영어식으로 포맷팅합니다.', () => {
+    expect(formatDate(new Date('2023-05-01'), 'DOT_EN')).toBe('05.01.2023');
   });
 
   test('날짜를 구분자 없이 8자리로 표시합니다.', () => {
