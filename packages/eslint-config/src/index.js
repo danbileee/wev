ed630e81a5714b634dd4fc5ef785efbee3da8ff2
Danbi Lee <hello@danbileee.com>
2025-02-26 14:15:16 +0900

    FIX: migrate to flat config

--- packages/eslint-config/src/index.js
+++ packages/eslint-config/src/index.js
@@ -1,55 +1,65 @@
-module.exports = {
-  env: {
-    es6: true,
-    node: true,
-    browser: true,
-  },
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:sonarjs/recommended',
-    'airbnb',
-    'plugin:prettier/recommended',
-  ],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-    project: ['./tsconfig.json'],
-  },
-  plugins: ['react', 'react-hooks', '@typescript-eslint', 'sonarjs'],
-  rules: {
-    'no-console': 'off',
-    'no-unused-vars': 'off',
-    'no-use-before-define': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'error',
-    'react/jsx-filename-extension': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'react/jsx-key': 'off',
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/require-default-props': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-var-requires': 'warn',
-    '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    'import/no-unresolved': 'off',
-    'import/extensions': 'off',
-    'import/no-anonymous-default-export': 'off',
-    'import/prefer-default-export': 'off',
-    'import/no-extraneous-dependencies': 'off',
-  },
+const plugin = {
+  configs: {},
+  rules: {},
+  processors: {},
 };
+
+Object.assign(plugin.configs, {
+  recommended: {
+    env: {
+      es6: true,
+      node: true,
+      browser: true,
+    },
+    extends: [
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:sonarjs/recommended',
+      'airbnb',
+      'plugin:prettier/recommended',
+    ],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+      project: ['./tsconfig.json'],
+    },
+    plugins: ['react', 'react-hooks', '@typescript-eslint', 'sonarjs'],
+    rules: {
+      'no-console': 'off',
+      'no-unused-vars': 'off',
+      'no-use-before-define': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+      'react/jsx-filename-extension': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/jsx-key': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/require-default-props': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-var-requires': 'warn',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'import/no-unresolved': 'off',
+      'import/extensions': 'off',
+      'import/no-anonymous-default-export': 'off',
+      'import/prefer-default-export': 'off',
+      'import/no-extraneous-dependencies': 'off',
+    },
+  },
+});
+
+export default plugin;
