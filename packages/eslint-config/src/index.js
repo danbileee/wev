8403c63037f31058829c8ab74f00308b398870b5
Danbi Lee <hello@danbileee.com>
2025-02-26 14:26:18 +0900

    ENH: install globals

--- packages/eslint-config/src/index.js
+++ packages/eslint-config/src/index.js
@@ -1,3 +1,5 @@
+import globals from 'globals';
+
 const plugin = {
   configs: {},
   rules: {},
@@ -6,10 +8,14 @@ const plugin = {
 
 Object.assign(plugin.configs, {
   recommended: {
-    env: {
-      es6: true,
-      node: true,
-      browser: true,
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        myCustomGlobal: 'readonly',
+      },
     },
     extends: [
       'plugin:@typescript-eslint/eslint-recommended',
@@ -27,14 +33,6 @@ Object.assign(plugin.configs, {
       },
     },
     parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 12,
-      sourceType: 'module',
-      project: ['./tsconfig.json'],
-    },
     plugins: ['react', 'react-hooks', '@typescript-eslint', 'sonarjs'],
     rules: {
       'no-console': 'off',
